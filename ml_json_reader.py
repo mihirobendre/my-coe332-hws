c624a76de1a2796256a6c55e60adcf24738fc2ce
Mihir Bendre <mihirobendre@utexas.edu>
2024-01-28 22:45:14 -0600

    some more progress

--- ml_json_reader.py
+++ ml_json_reader.py
@@ -1,4 +1,18 @@
 import json
 import pprint
+import numpy as np
 
-print("Hello")
+def summary_stats(a_list_of_dicts, a_key_string):
+    total_mass = 0.
+    for i in range(len(a_list_of_dicts)):
+        total_mass += float(a_list_of_dicts[i][a_key_string])
+mean_mass = (total_mass / len(a_list_of_dicts))
+print("Mean: " + mean_mass)
+
+
+with open('Meteorite_Landings.json', 'r') as f:
+    ml_data = json.load(f)
+
+#pprint.pprint(ml_data)
+
+
