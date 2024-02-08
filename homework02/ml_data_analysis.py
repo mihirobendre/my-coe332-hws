f8ce19bc79115a4ef9536c7ae21cb6802f7807cf
Mihir Bendre <mihirobendre@utexas.edu>
2024-02-08 10:47:09 -0600

    more progress

--- homework02/ml_data_analysis.py
+++ homework02/ml_data_analysis.py
@@ -1,15 +1,16 @@
 import csv
 import pprint
-
+import math
+import numpy as np
 
 #function definitions:
 
 def summary_stats(a_list_of_dicts, a_key_string):
-     total_mass = 0.0
+     total = 0.0
      median_list = []
  
      for i in range(len(a_list_of_dicts)):
-         total_mass += float(a_list_of_dicts[i][a_key_string])
+         total += float(a_list_of_dicts[i][a_key_string])
          median_list.append(float(a_list_of_dicts[i][a_key_string]))
  
      length = len(median_list)
@@ -18,9 +19,28 @@ def summary_stats(a_list_of_dicts, a_key_string):
      else:
          median_val = median_list[math.floor(length / 2)]
  
-     mean_mass = (total_mass / len(a_list_of_dicts))
-     print("Mean:", mean_mass)
+     mean_val = (total / len(a_list_of_dicts))
+     print("Mean:", mean_val)
      print("Median:", median_val)
+
+def remove_nulls(a_list_of_dicts, a_key_string):
+	new_list_of_dicts = []
+	for i in range(len(a_list_of_dicts)):
+		current_dict = a_list_of_dicts[i]
+		converted = True
+		try:
+			float_val = float(a_list_of_dicts[i][a_key_string])
+		except ValueError:
+			converted = False
+
+		if converted == True:
+			new_list_of_dicts.append(current_dict)
+	
+	return new_list_of_dicts
+	#pprint.pprint(new_list_of_dicts)
+
+#Distance Calculation Algorithm
+
 
 
 data = {}
@@ -31,13 +51,25 @@ with open('Meteorite_Landings_20240206.csv', 'r') as f:
 	reader = csv.DictReader(f)
 	for row in reader:
 		data['meteorite_landings'].append(dict(row))
-	 
-#summary_stats(data['meteorite_landings'], 'mass (g)')
+
+
+
+cleaned_data = remove_nulls(data['meteorite_landings'], 'mass (g)')
+
+ori_len = len(data['meteorite_landings'])
+new_len = len(cleaned_data)
+
+summary_stats(cleaned_data, 'mass (g)')
+
+#print("Original length: ", ori_len)
+#print("New length: ", new_len)
+
+#print("Diff: ", ori_len - new_len)
 
 #if __name__ == '__main__':
 #    main()
 
-pprint.pprint(data)
+#pprint.pprint(data)
 
 
 
