3e81aabe7df7a4d8a5ed0720eb52db46b6823094
Mihir Bendre <mihirobendre@utexas.edu>
2024-02-08 01:02:15 -0600

    updated

--- homework02/ml_data_analysis.py
+++ homework02/ml_data_analysis.py
@@ -15,10 +15,18 @@ import pprint
 data = {}
 data['meteorite_landings'] = []
 
-with open('Meteorite_Landings_20240206.csv', 'r') as f:
-    reader = csv.DictReader(f)
-    for row in reader:
-        data['meteorite_landings'].append(dict(row))
+
+
+
+def main():
+	with open('Meteorite_Landings_20240206.csv', 'r') as f:
+    	reader = csv.DictReader(f)
+    	for row in reader:
+        	data['meteorite_landings'].append(dict(row))
+
+if __name__ == '__main__':
+    main()
+
 
 #pprint.pprint(data)
 
