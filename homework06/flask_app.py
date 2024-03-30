1c23669ec9e0bdd82aa0dfc1f4cae2d6eacaa9da
Mihir Bendre <mihirobendre@utexas.edu>
2024-03-30 18:50:38 -0500

    updates

--- homework06/flask_app.py
+++ homework06/flask_app.py
@@ -1,22 +1,34 @@
+
 import requests
-import pprint
 from flask import Flask, request
 import redis
 import json
-
+from typing import Union, List, Dict
 
 app = Flask(__name__)
 
-rd = redis.Redis(host = 'redis-db', port = 6379, db = 0)
+rd: redis.Redis = redis.Redis(host='redis-db', port=6379, db=0)
 
 @app.route('/', methods=['GET'])
-def hello_world():
+def hello_world() -> str:
+    """
+    Root endpoint returning a simple greeting message.
+
+    Returns:
+        str: Greeting message.
+    """
     return 'Hello, world!\n'
 
-@app.route('/data', methods=['GET','POST','DELETE'])
-def handle_data():
+@app.route('/data', methods=['GET', 'POST', 'DELETE'])
+def handle_data() -> Union[str, List[Dict[str, str]]]:
+    """
+    Endpoint to handle data operations (GET, POST, DELETE).
+
+    Returns:
+        Union[str, List[Dict[str, str]]]: Response message or data.
+    """
     if request.method == 'POST':
-        # get data from web
+        # Get data from web
         url = "https://g-a8b222.dd271.03c0.data.globus.org/pub/databases/genenames/hgnc/json/hgnc_complete_set.json"
         response = requests.get(url)
         if response.status_code == 200:
@@ -25,34 +37,33 @@ def handle_data():
             data = full_data["response"]["docs"]
             my_id = 0
             for item in data:
-                
-                rd.set(my_id,json.dumps(item) )
+                rd.set(str(my_id), json.dumps(item))
                 my_id += 1
             return 'Data loaded\n'
         else:
             print("Failed to fetch data:", response.status_code)
-
     elif request.method == 'GET':
-        
         return_value = []
         for item in rd.keys():
             return_value.append(json.loads(rd.get(item)))
         return return_value
-
-        # iterate over keys in redis
-        # return everything as a json object
-
     elif request.method == 'DELETE':
-        # delete everything in redis
+        # Delete everything in redis
         for item in rd.keys():
             rd.delete(item)
         return 'Data deleted\n'
     else:
         return 'Not possible\n'
 
-@app.route('/gene',methods = ['GET'])
-def all_genes():
-    # get data from web
+@app.route('/gene', methods=['GET'])
+def all_genes() -> str:
+    """
+    Endpoint to retrieve information about all genes.
+
+    Returns:
+        str: JSON formatted list of gene IDs.
+    """
+    # Get data from web
     url = "https://g-a8b222.dd271.03c0.data.globus.org/pub/databases/genenames/hgnc/json/hgnc_complete_set.json"
     response = requests.get(url)
     list_of_genes = []
@@ -66,9 +77,18 @@ def all_genes():
     else:
         print("Failed to fetch data:", response.status_code)
 
-@app.route('/gene/<hgnc_id>',methods = ['GET'])
-def gene_info(hgnc_id):
-    # get data from web
+@app.route('/gene/<hgnc_id>', methods=['GET'])
+def gene_info(hgnc_id: str) -> Union[str, None]:
+    """
+    Endpoint to retrieve information about a specific gene.
+
+    Args:
+        hgnc_id (str): HGNC ID of the gene.
+
+    Returns:
+        Union[str, None]: JSON formatted gene information or None if not found.
+    """
+    # Get data from web
     url = "https://g-a8b222.dd271.03c0.data.globus.org/pub/databases/genenames/hgnc/json/hgnc_complete_set.json"
     response = requests.get(url)
     info_for_id = []
@@ -77,7 +97,6 @@ def gene_info(hgnc_id):
         full_data = response.json()
         data = full_data["response"]["docs"]
         for item in data:
-            #list_of_genes.append(item["hgnc_id"])
             if item["hgnc_id"] == hgnc_id:
                 info_for_id = json.dumps(item)
         return info_for_id
